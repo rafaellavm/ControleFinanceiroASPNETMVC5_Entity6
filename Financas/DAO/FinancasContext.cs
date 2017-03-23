7fd4f015857a95b4ccf438552ed6149f2b8f01cf
Rafaela <rafaela.vm@gmail.com>
2017-03-23 15:09:35 -0300

    02. Preparando o ambiente. Primeiro commit. Inicialização do projeto, instalação do Entity Framework 6.1.1. Criação das tabelas

---
+++ Financas/DAO/FinancasContext.cs
@@ -0,0 +1,24 @@
+﻿using Financas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Financas.DAO
+{
+    public class FinancasContext : DbContext
+    {
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Movimentacao> Movimentacoes { get; set; }
+
+        //A motivmentacao tem uma chave estrangeira para o usuario. Precisamos mapear essa chave dentro do Entity
+        //Precisaremos sobrescrever o método onModelCreate
+        //customiza o mapeamento pelo Entity
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //HHasRequired(m => m.Usuario): isso diz que a movimentação tem um usuário obrigatoriamente
+            modelBuilder.Entity<Movimentacao>().HasRequired(m => m.Usuario);
+        }
+    }
+}
---
+++ Financas/Entidades/Movimentacao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Financas.Entidades
+{
+    public class Movimentacao
+    {
+        public int ID { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime Data { get; set; }
+        public Tipo Tipo { get; set; }
+
+        public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+
+
+    }
+}
