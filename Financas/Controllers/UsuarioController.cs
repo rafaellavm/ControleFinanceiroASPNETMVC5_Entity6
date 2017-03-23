083fca5a2e1beda1bb4485f4405aa24490fb5116
Rafaela <rafaela.vm@gmail.com>
2017-03-23 20:16:34 -0300

    03 Layout e cadastro de usuários

---
+++ Financas/Controllers/UsuarioController.cs
@@ -0,0 +1,45 @@
+﻿using Financas.DAO;
+using Financas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Financas.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private UsuarioDAO usuariodao;
+
+        public UsuarioController(UsuarioDAO usuariodao)
+        {
+            this.usuariodao = usuariodao;
+        }
+        
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        public ActionResult Adiciona(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                usuariodao.Adiciona(usuario);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Form", usuario);
+            }
+
+        }
+
+        public ActionResult Index()
+        {
+            IList<Usuario> usuarios = usuariodao.Lista();
+            return View(usuarios);
+        }
+    }
+}
