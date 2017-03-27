0bcd02df9a538499f96056766bb416caee145502
Rafaela <rafaela.vm@gmail.com>
2017-03-27 17:50:30 -0300

    6 Gerando relatórios

--- Financas/Controllers/MovimentacaoController.cs
+++ Financas/Controllers/MovimentacaoController.cs
@@ -1,5 +1,6 @@
 ﻿using Financas.DAO;
 using Financas.Entidades;
+using Financas.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -43,6 +44,23 @@ namespace Financas.Controllers
                 ViewBag.Usuarios = usuarioDAO.Lista();
                 return View("Form");
             }
+        }
+
+        //? porque pode não ter valor
+        public ActionResult MovimentacoesPorUsuario(MovimentacoesPorUsuarioModel model)
+        {
+            model.Usuarios = usuarioDAO.Lista();
+            model.Movimentacoes = movimentacaoDAO.BuscaPorUsuario(model.UsuarioId);
+            return View(model);
+        }
+
+        public ActionResult Busca(BuscaMovimentacoesModel model)
+        {
+            model.Usuarios = usuarioDAO.Lista();
+            model.Movimentacoes = movimentacaoDAO.Busca(model.ValorMinimo, model.ValorMaximo, model.DataMinima, 
+                                                        model.DataMaxima, model.Tipo, model.UsuarioId);
+
+            return View(model);
         }
     }
 }
--- Financas/DAO/MovimentacaoDAO.cs
+++ Financas/DAO/MovimentacaoDAO.cs
@@ -25,5 +25,43 @@ namespace Financas.DAO
         {
             return context.Movimentacoes.ToList();
         }
+
+        public IList<Movimentacao> BuscaPorUsuario(int? usuarioId)
+        {
+            return context.Movimentacoes.Where(m => m.UsuarioId == usuarioId).ToList();
+        }
+
+        internal IList<Movimentacao> Busca(decimal? ValorMinimo, decimal? ValorMaximo, DateTime? DataMinima, DateTime? DataMaxima, 
+                                           Tipo? tipo, int? usuarioId)
+        {
+            //IQueryable<T> que é específico para o LINQ
+            IQueryable<Movimentacao> busca = context.Movimentacoes;
+            if(ValorMinimo.HasValue){
+
+                busca = busca.Where(m => m.Valor >= ValorMinimo);
+            }
+            if (ValorMaximo.HasValue)
+            {
+                busca = busca.Where(m => m.Valor <= ValorMaximo);
+            }
+            if (DataMinima.HasValue)
+            {
+                busca = busca.Where(m => m.Data >= DataMinima);
+            }
+            if (DataMaxima.HasValue)
+            {
+                busca = busca.Where(m => m.Data <= DataMaxima);
+            }
+            if (tipo.HasValue)
+            {
+                busca = busca.Where(m => m.Tipo == tipo);
+            }
+            if (usuarioId.HasValue)
+            {
+                busca = busca.Where(m => m.UsuarioId == usuarioId);
+            }
+
+            return busca.ToList();
+        }
     }
 }
